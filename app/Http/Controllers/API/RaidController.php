2005d754a919c7607fea9ca7b42dbaa2d9dc8ea9
inyerman-f <Yeka12!21>
2019-11-01 15:55:47 -0400

    update

--- app/Http/Controllers/API/RaidController.php
+++ app/Http/Controllers/API/RaidController.php
@@ -38,7 +38,7 @@ class RaidController extends Controller
             $end_time = $end_time->add(new DateInterval('PT' .($request->end_time + 45).'M'));
         }else {
             $hatch_time = new DateTime();
-            $hatch_time = $hatch_time->add(new DateInterval('PT' .($request->end_time - 45).'M'));
+           // $hatch_time = $hatch_time->add(new DateInterval('PT' .($request->end_time - 45).'M'));
             $end_time = new DateTime();
             $end_time = $end_time->add(new DateInterval('PT' . $request->end_time . 'M'));
         }
