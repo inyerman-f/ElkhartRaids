f8ea51c1f7584fad1e80c3820d833be3d5292ced
machoratonenturcado <elwewenzenica@gmail.com>
2019-11-04 15:16:49 -0500

    update

--- resources/views/chunks/seo/seoTags.blade.php
+++ resources/views/chunks/seo/seoTags.blade.php
@@ -1,5 +1,5 @@
-        <meta property="og:url"                content="http://172.104.152.57/@yield('relativepath')" />
-        <meta property="og:type"               content="@yield('page-type')" />
-        <meta property="og:title"              content="@yield('title')" />
-        <meta property="og:description"        content="@yield('Description')" />
-        <meta property="og:image"              content="@yield('image')" />
+    <meta property="og:url"                content="https://elkhartraids.website/@yield('relativepath')" />
+    <meta property="og:type"               content="@yield('page-type')" />
+    <meta property="og:title"              content="@yield('title')" />
+    <meta property="og:description"        content="@yield('Description')" />
+    <meta property="og:image"              content="@yield('image')" />
