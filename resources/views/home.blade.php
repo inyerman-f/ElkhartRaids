455646691ad7d8d650b1513a7bcabd9864665168
machoratonenturcado <elwewenzenica@gmail.com>
2019-11-04 15:48:32 -0500

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,17 @@
                 version          : 'v3.3'
             });
         };
-
+        FB.AppEvents.logPageView();
         (function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
             js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
             fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+        }(document, 'script', 'facebook-jssdk'));
+
+
+    </script>
 
     <!-- Your customer chat code -->
     <div class="fb-customerchat"
