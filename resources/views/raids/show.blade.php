66413dff4ec4d847c633f1b9629b543712461b9e
machoratonenturcado <elwewenzenica@gmail.com>
2019-11-05 12:21:15 -0500

    update

--- resources/views/raids/show.blade.php
+++ resources/views/raids/show.blade.php
@@ -132,7 +132,7 @@
                 // The map, centered at Uluru
                 var map = new google.maps.Map(
                     document.getElementById('map'), {zoom: 15, center: uluru});
-                var icono = { {{$raid->boss_image}} };
+                var icono = '{{$raid->boss_image}}';
                 // The marker, positioned at Uluru
                 var marker = new google.maps.Marker({
                     position: uluru,
